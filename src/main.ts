048ad0698af3741f5c5fe658a44a167e1e6d20ee
baijanathTharu <bnchaudhary.tharu@gmail.com>
2024-03-09 21:49:32 +0545

    setup socket io

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log("main hot reload");
+import express from "express";
+import { createServer } from "node:http";
+import { join } from "node:path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(join(process.cwd(), "templates/index.html"));
+});
+
+server.listen(4000, () => {
+  console.log("server running at http://localhost:4000");
+});
